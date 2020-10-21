45fb1b97a0576f68cbaaa9448e83b7b4d6f7a3b2
B Akhil <36125249+burugupallymatam@users.noreply.github.com>
2020-10-21 13:03:51 +0530

    Add files via upload

---
+++ Nibba.py
@@ -0,0 +1,9 @@
+#Enter your age when dialoge box appears to check whether you're nibba or not
+Age=int(input())
+if Age >= 18:
+   print("You are not nibba")
+else:
+   print("You are Nibba")
+
+
+#In telugu states of India, Children below age of 18 are called Nibba by 90's ids
